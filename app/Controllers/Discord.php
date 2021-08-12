0a84781e12ba2316d181c8d8620a55165137cd07
tran <lytranuit@gmail.com>
2021-08-12 18:51:47 +0700

    update

---
+++ app/Controllers/Discord.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controllers;
+
+class Discord extends BaseController
+{
+    protected $auth;
+    /**
+     * @var Auth
+     */
+    protected $config;
+
+    /**
+     * @var \CodeIgniter\Session\Session
+     */
+    protected $session;
+
+    public function __construct()
+    {
+        // Most services in this controller require
+        // the session to be started - so fire it up!
+        // $this->session = service('session');
+
+        // $this->config = config('Auth');
+        // $this->auth = service('authentication');
+    }
+
+    public function index()
+    {
+        return redirect()->to("https://discord.com/invite/7TeAJvztmH");
+    }
+}
