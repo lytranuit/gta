79b079ffd88992c244ef7a10269aa5b0e07b3885
tran <lytranuit@gmail.com>
2021-08-16 00:58:52 +0700

    update

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -16,12 +16,12 @@ if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
  * Router Setup
  * --------------------------------------------------------------------
  */
-// $routes->setDefaultNamespace('App\Controllers');
-// $routes->setDefaultController('Home');
-// $routes->setDefaultMethod('index');
-// $routes->setTranslateURIDashes(false);
-// $routes->set404Override();
-// $routes->setAutoRoute(true);
+$routes->setDefaultNamespace('App\Controllers');
+$routes->setDefaultController('Home');
+$routes->setDefaultMethod('index');
+$routes->setTranslateURIDashes(false);
+$routes->set404Override();
+ $routes->setAutoRoute(true);
 
 /**
  * --------------------------------------------------------------------
@@ -31,7 +31,7 @@ if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
+//$routes->get('/', 'Home::index');
 //$routes->get('/lang/{locale}', 'Language::index');
 $routes->add('lang/(:any)', 'Language::index/$1');
 
